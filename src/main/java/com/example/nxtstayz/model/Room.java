85c1de0e70de991042d57399549326ba74dc7ee3
= <sreekanthgithub1@gmail.com>
2023-12-12 05:05:04 +0000

    solution

---
+++ src/main/java/com/example/nxtstayz/model/Room.java
@@ -0,0 +1,80 @@
+package com.example.nxtstayz.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "room")
+public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Column(name = "roomid")
+    private int roomId;
+
+    @Column(name = "roomname")
+    private String roomName;
+
+    @Column(name = "type")
+    private String type;
+
+    @Column(name = "price")
+    private double price;
+
+    @ManyToMany
+    @JoinColumn(name = "hotelid")
+    private Hotel hotel;
+
+    public Room() {
+
+    }
+
+    public Room(int roomId, String roomName, String type, double price, Hotel hotel) {
+        this.roomId = roomId;
+        this.roomName = roomName;
+        this.type = type;
+        this.price = price;
+        this.hotel = hotel;
+    }
+
+    public int getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(int roomId) {
+        this.roomId = roomId;
+    }
+
+    public String getRoomName() {
+        return roomName;
+    }
+
+    public void setRoomName(String roomName) {
+        this.roomName = roomName;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Hotel getHotel() {
+        return hotel;
+    }
+
+    public void setHotel(Hotel hotel) {
+        this.hotel = hotel;
+    }
+
+}
